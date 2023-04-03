d7f4a9646e4a619a627a38458f69559dcb15908e
Mayank tiwari <mt1027319@gmail.com>
2023-04-03 23:37:45 +0530

    Calculator js complete

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let string = "";
-let button = document.querySelectorAll('.button');
-Array.button
+let result = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      result = eval(result);
+      document.querySelector('input').value = result;
+    }
+    else if(e.target.innerHTML == 'AC'){
+      result = "";
+      document.querySelector('input').value = result;
+    }else if(e.target.innerHTML == 'C'){
+      result = result.slice(0, -1);
+        document.querySelector('input').value = result;
+    }
+    else{ 
+      result = result + e.target.innerHTML;
+    document.querySelector('input').value = result;
+      }
+  })
+})
